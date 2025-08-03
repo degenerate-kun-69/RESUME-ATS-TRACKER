6f240f1fe2babb0515f93a476472d8e462241d9c
Srrijan Arora <sabhaya5567@gmail.com>
2025-08-03 21:12:57 +0530

    windows fix

--- app.py
+++ app.py
@@ -10,3 +10,20 @@ from langchain_google_genai import GoogleGenerativeAI
 import google.generativeai as genai
 import faiss
 import pymupdf
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+def get_gemini_response(input,pdf_content,prompt):
+    model=genai.GenerativeModel(
+        model_name='gemini-2.5-pro'
+    )
+    generation_config ={
+        "temperature": 0.15,
+        "top_p": 0.95,
+        "max_output_tokens": 1024,
+        "stop_sequences": ["\n"],
+        "top_k": 40
+    }
+    response=model.generate_content(
+        [input,pdf_content[0],prompt],
+        generation_config=generation_config
+    )
+    return response.text
